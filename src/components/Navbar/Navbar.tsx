6581e3bb9e7885c3cb1ea62a71920965c1328e2b
Malith Anjana <malithanjana97@gmail.com>
2023-06-24 20:39:26 +0530

    Completed Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,13 @@
 import DevicesOtherIcon from "@mui/icons-material/DevicesOther";
-import { AppBar, Typography, InputBase, Avatar } from "@mui/material";
+import { AppBar, Typography, InputBase, Avatar, Menu, MenuItem } from "@mui/material";
 import { Icons, Search, StyledBadge, StyledToolbar, UserBox } from "./styled.component";
 import IconButton from '@mui/material/IconButton';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import { theme } from "../../theme/theme";
-
+import {useState} from 'react'
 const Navbar = () => {
+  const [open, setOpen] = useState<boolean>(false);
   return (
     <AppBar sx={{ bgcolor: "" }} position="sticky">
       <StyledToolbar>
@@ -32,15 +33,35 @@ const Navbar = () => {
               < FavoriteIcon sx={{color:theme.palette.primary.light}}/>
             </StyledBadge>
           </IconButton>
-          <Avatar sx={{ width: 30, height: 30 }} alt="Remy Sharp" src="https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60" />
+          <Avatar onClick={()=> setOpen(true)} sx={{ width: 30, height: 30 }} alt="Remy Sharp" src="https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60" />
         
         </Icons>
         <UserBox>
-        <Avatar sx={{ width: 30, height: 30 }} alt="Remy Sharp" src="https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60" />
+        <Avatar onClick={()=> setOpen(true)} sx={{ width: 30, height: 30 }} alt="Remy Sharp" src="https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60" />
         <Typography>John</Typography>
         </UserBox>
         
       </StyledToolbar>
+      <Menu
+        id="basic-menu"
+        open={open}
+        onClose={()=> setOpen(false)}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+        anchorOrigin={{
+          vertical:'top',
+          horizontal:'right'
+        }}
+        transformOrigin={{
+          vertical:'top',
+          horizontal:'right'
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
 };
